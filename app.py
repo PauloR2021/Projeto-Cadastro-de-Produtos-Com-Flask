bf0f69b323b38974365f7c852978425997905cb8
PauloR2021 <paulotrindadeoficial@gmail.com>
2025-06-28 22:04:05 -0300

    Add: Criando o arquivo principal do aplicativo em Python e adicionando o arquivo das bibliotecas

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, redirect
+from flask_mysqldb import MySQL
+from dotenv import load_dotenv
+import os
+
+
+#Importando os dados do .Env
+load_dotenv()
+
+app = Flask(__name__)
+
+# Configurações do banco a partir do .env
+app.config['MYSQL_HOST'] = os.getenv("MYSQL_HOST")
+app.config['MYSQL_USER'] = os.getenv("MYSQL_USER")
+app.config['MYSQL_PASSWORD'] = os.getenv("MYSQL_PASSWORD")
+app.config['MYSQL_DB'] = os.getenv("MYSQL_DB")
+
+mysql = MySQL(app)
+
+print(mysql)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
